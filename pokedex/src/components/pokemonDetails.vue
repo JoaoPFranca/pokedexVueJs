b34cd2ab0d02b13f85ef5b283f053455d8b10e5f
JoaoPFranca <jpbarboza2004@gmail.com>
2023-08-11 11:50:03 -0300

    The 2 main moves on the details

--- pokedex/src/components/pokemonDetails.vue
+++ pokedex/src/components/pokemonDetails.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="background-container">
     <v-container>
-      <div style="margin-top: 4em;">
-        <img :src="pokemonImageUrl" id="imagem" style="width: 400px; text-align: center;" />
+      <div style=" margin: 0; position: absolute; top: 43%; -ms-transform: translateY(-50%); transform: translateY(-50%);">
+        <v-img :src="pokemonImageUrl" id="imagem" style="width: 400px; text-align: center;" />
+      </div>
+
+      <div style=" margin: 0; position: absolute; top: 22%; font-size: 32px; margin-left: 24em;">
+        <h2>{{ pokemonId }} - {{ pokemonName }}</h2>
+      </div>
+
+      <div style=" margin: 0; position: absolute; top: 47%; font-size: 28px; margin-left: 31em;">
+        <h2>{{ pokemonMove1 }}</h2>
+      </div>
+
+      <div style=" margin: 0; position: absolute; top: 55.5%; font-size: 28px; margin-left: 31em;">
+        <h2>{{ pokemonMove2 }}</h2>
       </div>
     </v-container>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {
       pokemonId: null,
       pokemonImageUrl: null,
+      pokemonName: null,
+      pokemonMove1: null,
+      pokemonMove2: null,
     };
   },
 
@@ -23,12 +40,25 @@ export default {
       const pokemonId = parts[parts.length - 1];
       return pokemonId;
     },
+
+    getPokemon () {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${this.pokemonId}`).then((resp) => {
+        this.pokemonName = resp.data.name.toUpperCase();
+        if (resp.data.moves.length >= 1) {
+          this.pokemonMove1 = resp.data.moves[0].move.name.toUpperCase();
+        }
+        if (resp.data.moves.length >= 2) {
+          this.pokemonMove2 = resp.data.moves[1].move.name.toUpperCase();
+        }
+    });
+    },
   },
 
   mounted() {
     const currentUrl = window.location.href;
     this.pokemonId = this.extractPokemonIdFromUrl(currentUrl);
     this.pokemonImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokemonId}.png`;
+    this.getPokemon();
   },
 };
 </script>
